ca3d197eb4054dd3d5b72508e8726d66b04a441d
LorenaJeger <57949840+LorenaJeger@users.noreply.github.com>
2020-04-29 11:33:55 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,55 +1,264 @@
 import express from 'express';
-import storage from './memory_storage'
+import storage from './memory_storage.js'
 import cors from 'cors'
+import connect from './db.js'
+import mongo from 'mongodb'
 
-const app = express()  // instanciranje aplikacije 
-app.use(cors())
-
+const app = express()  // instanciranje aplikacije
 const port = 3000  // port na kojem će web server slušati
 
+app.use(cors())
+app.use(express.json()) // automatski dekodiraj JSON poruke
 
-app.get('/posts', (req, res) => {
-    let title= req.query.title
-    let createdBy=req.query.createdBy
-    let query = req.query
-
-    let postovi= storage.posts
-    if(title){
-
-    postovi=postovi.filter(e =>{
-        
-        let titleOk = e.title.indexOf(title) >=0 
-        return titleOk;
-    })
-
-    }  
-    if (createdBy){
-        postovi=postovi.filter(e => {
-            let createdByOk= e.createdBy.indexOf(createdBy)>= 0
-            return createdByOk;  
-        })
-        
+let checkAttributes= (data)=>{
+    if (!data.createdBy || !data.title || !data.source){
+        return false
     }
+    return true
+};
 
-    if (query._any) {
-        let pretraga= query._any
-        let pojmovi = pretraga.split(" ")
-        console.log(pojmovi)
+/* let checkAttributes_mail=(data)=>{
+    let mailTest = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$', 'g');
+    if(!mailTest.test(data.createdBy)) {
        
-        postovi=postovi.filter(postovi=>{
-            let podaci= postovi.title + postovi.createdBy
-            let rezlutat= pojmovi.every(pojam =>{                   //every vraca true ako svaka podfunkcija vraca true
-                    return podaci.indexOf(pojam) >=0 
-            })
-            return rezlutat
+        return false
+    }
+   return true
+}; */
+
+    
+   
+app.patch('/posts/:id',async (req,res )=> {
+    
+    let id= req.params.id;
+    let data= req.body;
+    
+   
+    delete data._id;
+
+   let db = await connect();
+
+    let result= await db.collection("posts").updateOne(
+        {_id:  mongo.ObjectId(id) }, 
+        {
+            $set: data  
+        });
+
+    if(result && result.modifiedCount == 1){
+        let doc= await db.collection("posts").findOne(
+            {_id:  mongo.ObjectId(id)});
+
+            res.json(doc);
+        } 
+    else {
+        res.json({
+            status: "fail",
+        });
+    }
+});
+
+app.put('/posts/:id',async (req,res )=> {
+    
+    let id= req.params.id;
+    let data= req.body;
+    
+    data.postedAt= new Date().getTime();
+   
+    delete data._id;
+
+    
+   let check= checkAttributes(data);
+
+   if(!check){
+       res.json({
+           status: "fail",
+           reason:"incomplete post",
+       });
+       return ;
+   }
+
+   let db = await connect();
+
+    let result= await db.collection("posts").replaceOne(
+        {_id:  mongo.ObjectId(id) }, data);
+
+    if(result && result.modifiedCount == 1){
+       let  returnData= result.ops[0]
+       returnData._id=id;
+       res.json(returnData)
+    }
+    else {
+        res.json({
+            status: "fail",
+        });
+    }
+});
+
+
+    
+//insert
+app.post('/posts', async (req, res) => {
+    let data= req.body;
+    //postavi vrijeme i datum posta
+    data.postedAt= new Date().getTime();
+    //zelimo validan id pa pustamo da ga mongo postavi
+    delete data._id;
+
+    
+    let check= checkAttributes(data);
+    if(!check){
+        res.json({
+            status: "fail",
+            reason:"incomplete post",
+        });
+        return 
+    }
+    
+   /*  let check_mail= checkAttributes_mail(data);
+    if(!check_mail){
+        res.json({
+            status: "Erorr ",
+            reason:"Neispravan createdBy",
+        });
+        return 
+    } */
+   
+
+
+    
+//stavimo konekciju prema bazi
+    let db= await connect();
+
+    let result= await db.collection("posts").insertOne(data);
+    console.log(result);
+
+    if(result && result.insertedCount == 1){
+        res.json(result.ops[0]);
+    }
+    else {
+        res.json({
+            status: "fail",
+        });
+    }
+});
+
+//dohvacanje posta po id-u
+app.get('/posts/:id',async (req,res )=> {
+    let id= req.params.id;
+    let db = await connect();
+    
+    let doc= await db.collection("posts").findOne({_id:  mongo.ObjectId(id)});
+    console.log(doc);
+    res.json(doc);
+   
+});
+
+//dohvacanje random posta
+app.get('/posts_rand', async (req,res )=> {
+    let db = await connect();
+    let document = await db.collection('posts').aggregate([{$sample: { size: 1 }}]);
+    let result= await document.toArray();
+
+    res.json(result[0]);
+   
+});
+
+app.get('/posts', async (req, res) => {
+  let db = await connect();
+  let query = req.query;
+
+  let selekcija = {};
+
+  if (query._any) {
+    // za upit: /posts?_all=pojam1 pojam2
+    let pretraga = query._any;
+    let terms = pretraga.split(' ');
+
+    let atributi = ['title', 'createdBy'];
+
+    selekcija = {
+      $and: [],
+    };
+
+    terms.forEach((term) => {
+      let or = {
+        $or: [],
+      };
+
+      atributi.forEach((atribut) => {
+        or.$or.push({ [atribut]: new RegExp(term) });
+      });
+
+      selekcija.$and.push(or);
+    });
+  }
+
+  console.log('Selekcija', selekcija);
+
+  let cursor = await db.collection('posts').find(selekcija);
+  let results = await cursor.toArray();
+
+  res.json(results);
+});
+
+app.get('/posts_memory', (req, res) => {
+
+    let posts = storage.posts
+    let query = req.query
+    
+    if (query.title) {
+        posts = posts.filter(e => e.title.indexOf(query.title) >= 0)
+    }
+    
+    if (query.createdBy) {
+        posts = posts.filter(e => e.createdBy.indexOf(query.createdBy) >= 0)
+    }
+    
+    if (query._any) {
+        let terms = query._any.split(" ")
+        posts = posts.filter(doc => {
+            let info = doc.title + " " + doc.createdBy
+            return terms.every(term => info.indexOf(term) >= 0)
         })
     }
 
+    // sortiranje
+    posts.sort((a, b) => (b.postedAt - a.postedAt))
 
+    res.json(posts)
+});
 
-res.json(postovi)
+app.post('/posts/:postId/comments', async (req, res) => {
+    let db = await connect();
+    let doc = req.body;
+    let postId = req.params.postId;
+
+    //u Mongu dokumenti unutar postojećih dokumenata ne dobivaju automatski novi _id
+    //pa ga moramo dodati sami
+    doc._id = mongo.ObjectId();
+
+    //datume je ispravnije definirati na backendu
+    doc.posted_at = Date.now();
+
+    let result = await db.collection('posts').updateOne(
+        { _id: mongo.ObjectId(postId) },
+        {
+           //operacija $push dodaje novu vrijednost u atribut 'comments'
+           //ako on ne postoji automatski ga stvara i postavlja na []
+           $push: { comments: doc }, 
+        }
+    );
+    if(result.modifiedCount == 1) {
+        res.json({
+            status: 'success',
+            id: doc._id,    //kao id vraćamo generirani _id
+        });
+    } else {
+        res.statusCode = 500;
+        res.json({
+            status: 'fail',
+        });
+    }
 });
 
 app.listen(port, () => console.log(`Slušam na portu ${port}!`))
-
-    
